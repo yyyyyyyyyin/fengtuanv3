08adf5059bb9cc40f5536a9074b5cc12f2d227bf
yinshengqiang <1138444018@qq.com>
2023-02-28 16:29:25 +0800

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true,
+	transpileDependencies: true,
+	// 全局样式↓=====================================================
+	css: {
+		loaderOptions: {
+			scss: {
+				additionalData: `@import "@/assets/css/variables.scss";`,
+				// 旧版本用prependData，新版比如cli5本用additionalData
+			},
+		},
+	},
+	// 全局样式↑=====================================================
+
+	// 全局标题↓=====================================================
+	chainWebpack: (config) => {
+		config.plugin('html').tap((args) => {
+			args[0].title = '锋团运营平台';
+			return args;
+		});
+	},
+	// 全局标题↑=====================================================
 });
